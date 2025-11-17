5ac631673180fd4b5be161ed75193ab4ba9f8f2d
Rakzmey <rakzmey@gmail.com>
2025-11-17 22:22:11 +0700

    FIX ADSENSE

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -26,7 +26,7 @@ class MyDocument extends Document {
             }
           `}</style>
           <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1387523749542047"
-            crossorigin="anonymous"></script>
+            crossOrigin="anonymous"></script>
         </Head>
         <body>
           <script dangerouslySetInnerHTML={{
